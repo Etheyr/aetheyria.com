7928416c825328496c3ef97c364b0792197d7339
Axel Bouillart <axel.bouillart@gmail.com>
2020-02-01 01:16:02 +0100

    Update about.blade.php

---
+++ resources/views/about.blade.php
@@ -0,0 +1,33 @@
+@extends('index')
+
+@section('apropos')
+
+<!-- About Section -->
+<section class="page-section bg-primary text-white mb-0" id="about">
+  <div class="container">
+
+    <!-- About Section Heading -->
+    <h2 class="page-section-heading text-center text-uppercase text-white">A propos</h2>
+
+    <!-- Icon Divider -->
+    <div class="divider-custom divider-light">
+      <div class="divider-custom-line"></div>
+      <div class="divider-custom-icon">
+        <i class="fas fa-star"></i>
+      </div>
+      <div class="divider-custom-line"></div>
+    </div>
+
+    <!-- About Section Content -->
+    <div style="text-align:justify" class="row">
+      <div class="col-lg-4 ml-auto">
+        <p class="lead">En formation de graphiste à BA13 Lanton j'ai créer mon propre site pour mettre mes créations en avant et  mon projet personnel de fin de formations. </p>
+      </div>
+      <div class="col-lg-4 mr-auto">
+        <p class="lead">Vous trouverez sur mon site des  <strong>logos</strong> et illustrations que j'ai pu créer, et des exemples de chartes graphiques  pour les professionels(les)</p>
+      </div>
+    </div>
+
+  </div>
+</section>
+@stop
